a23af54ad97e888b2008c2b82688ecffec347fef
Justin Singh <justin.singh125125@gmail.com>
2018-11-11 11:21:02 -0800

    added routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,22 @@
-.get('admin/asdasd',werwerwr{
+//include the path package so we can gtet the correct file path for our html
+var path = require("path");
 
-})
+//we are going to be exporting so the server can include this file
 
-.get('admin/dfdgfdg')
+module.exports = function (app) {
 
+    //the code below handles when a user requests to  visit a page
+
+
+
+    //if this is hit, will take user to the survey page
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+
+    //catches all input and if not one of the listen above, takes to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
+}
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var express = require("express");
 
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+//set up express to allow data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//point our server as specific locations to access the urls
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//make our server listen so it can respond to what the user wants
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+})
