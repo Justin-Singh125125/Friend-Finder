4452ad11e654edee114159696233a5aee0c37e57
Justin Singh <justin.singh125125@gmail.com>
2018-11-11 14:12:36 -0800

    added support for correct friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,11 @@ var friends = [
         name: "Justin",
         photo: "https://www.wsupercars.com/wallpapers/Tesla/2017-Tesla-Model-S-P100D-V1-1080.jpg",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Jenna",
+        photo: "https://i.ytimg.com/vi/kYnx7nPw-xo/maxresdefault.jpg",
+        scores: [1, 5, 3, 2, 3, 1, 2, 3, 4, 4]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,43 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
+        var compareUser = req.body;
+        var temp;
+        var currentMatchIndex;
+        var lowestDifference;
+        var totalDifference;
+        var isFirst = true;
+        //a nested for loop to check all array and scores
+
+        for (var i = 0; i < friendsData.length; i++) {
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                if (compareUser.scores[j] != friendsData[i].scores[j]) {
+                    //subtracts the two scores from each index and stores in temp so we can check for negative values
+                    temp = compareUser.scores[j] - friendsData[i].scores[j];
+                    //if negative multiply by negative 1 to get a positive
+                    if (temp < 0) {
+                        temp *= -1;
+                    }
+                    //add temp to total difference
+                    totalDifference += temp;
+                }
+                //first time around, set set the first i(0) to the lowestDifference and current match index
+                if (isFirst) {
+                    lowestDifference = totalDifference;
+                    currentMatchIndex = i;
+                    isFirst = false;
+                }
+                //since we previosly set lowest difference to a value we can further compare that value to the current total difference
+                if (totalDifference < lowestDifference) {
+                    lowestDifference = totalDifference;
+                    currentMatchIndex = i;
+                }
+
+            }
+        }
         //we need to push the json to the array storage 
         friendsData.push(req.body);
+        res.json(friendsData[currentMatchIndex]);
+        // console.log(friendsData);
     })
 }
