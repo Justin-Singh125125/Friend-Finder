2a2e4fa32232e9607145f9616770900e4bcc337f
Justin Singh <justin.singh125125@gmail.com>
2018-11-11 15:40:46 -0800

    added dummy values

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,35 @@
-var friends = [];
+var friends = [
+    {
+        name: "Alex",
+        photo: "http://www.j6design.com.au/wp-content/uploads/2015/03/SMART-goals.png",
+        "scores": ["5", "1", "3", "5", "2", "4", "3", "2", "4", "1"]
+    },
+    {
+        name: "Michael",
+        photo: "https://3vnqw32fta3x1ysij926ljs3-wpengine.netdna-ssl.com/wp-content/uploads/2003/03/Talking-Trash-576x360.jpg",
+        "scores": ["4", "5", "3", "2", "4", "1", "4", "2", "4", "2"]
+    },
+    {
+        name: "Danny",
+        photo: "https://images.8tracks.com/cover/i/000/173/224/sick__dude_slang_frame-4752.jpg?rect=59,0,460,460&q=98&fm=jpg&fit=max",
+        "scores": ["5", "4", "2", "3", "4", "1", "1", "4", "5", "4"]
+    },
+    {
+        name: "Aman",
+        photo: "http://cdn.shopify.com/s/files/1/0385/6229/files/Tomorrowland-1200x520.jpg?v=1514077842",
+        "scores": ["4", "3", "1", "5", "5", "3", "4", "5", "2", "3"]
+    },
+    {
+        name: "Janessa",
+        photo: "https://www.chemionglasses.com/wp-content/uploads/2016/06/edm-massive-festival.png",
+        "scores": ["1", "3", "1", "5", "2", "1", "5", "2", "3", "4"]
+    },
+    {
+        name: "Justin",
+        photo: "https://techcrunch.com/wp-content/uploads/2018/03/tesla-model-3-12.jpg?w=940&h=465&crop=1",
+        "scores": ["5", "3", "4", "1", "4", "1", "3", "1", "4", "5"]
+    },
+];
 
 //exporting friends array
 module.exports = friends;
