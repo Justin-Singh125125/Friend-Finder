1ae2cccc40c372330aaecefc44e616472c065101
Justin Singh <justin.singh125125@gmail.com>
2018-11-11 15:18:53 -0800

    fixed mobile device issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,4 @@
-var friends = [
-    {
-        name: "Justin",
-        photo: "https://www.wsupercars.com/wallpapers/Tesla/2017-Tesla-Model-S-P100D-V1-1080.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    },
-    {
-        name: "Jenna",
-        photo: "https://i.ytimg.com/vi/kYnx7nPw-xo/maxresdefault.jpg",
-        scores: [1, 5, 3, 2, 3, 1, 2, 3, 4, 4]
-    },
-    {
-        name: "test",
-        photo: "https://www.compasspoint.org/sites/default/files/images/test.png",
-        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
-    }
-];
+var friends = [];
 
 //exporting friends array
 module.exports = friends;
